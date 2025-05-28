29ea0adf9408f2b8747433425f727c5c12603b72
Lucian Chauvin <lucianchauvin@gmail.com>
2025-05-27 22:41:02 -0500

    uci load poptactoe

--- src/emscripten/public/uci.js
+++ src/emscripten/public/uci.js
@@ -2,8 +2,33 @@ const process = require("process");
 const fs = require("fs");
 const readline = require("readline");
 const Stockfish = require("./stockfish.js");
+let stockfish;
+const poptactoe_variant = `[poptactoe]
+maxRank = 8
+maxFile = 8
+immobile = R
+startFen = 8/8/8/8/8/8/8/8[RRRRRRRRrrrrrrrr] w - - 0 1
+mutuallyImmuneTypes = R
+pieceDrops = true
+mustDrop = true
+doubleStep = false
+castling = false
+stalemateValue = draw
+immobilityIllegal = false
+connectN = 3
+promotionRegionWhite = -
+promotionRegionBlack = -`;
 
 const UCI_NNUE_FILE = process.env.UCI_NNUE_FILE;
+
+async function loadPopVariant(poptactoe_variant) {
+  if (!poptactoe_variant) return;
+
+  const FS = stockfish.FS;
+  const filename = "/poptactoe.ini";
+  FS.writeFile(filename, poptactoe_variant);
+  stockfish.postMessage(`load ${filename}`);
+}
 
 async function runRepl(stockfish) {
   const iface = readline.createInterface({ input: process.stdin });
@@ -17,7 +42,7 @@ async function runRepl(stockfish) {
 }
 
 async function main(argv) {
-  const stockfish = await Stockfish();
+  stockfish = await Stockfish();
   const FS = stockfish.FS;
   if (UCI_NNUE_FILE) {
     const buffer = await fs.promises.readFile(UCI_NNUE_FILE);
@@ -33,6 +58,8 @@ async function main(argv) {
     stockfish.postMessage("quit");
     return;
   }
+
+  loadPopVariant(poptactoe_variant);
   runRepl(stockfish);
 }
 
